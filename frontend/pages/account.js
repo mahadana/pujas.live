ac9b55eaf00e1c7e554c54dcbb2782b15211a504
Jāgaro <v.jagaro@gmail.com>
2020-12-20 03:03:45 -0800

    Add video to group

--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,22 +1,12 @@
-import { gql, useQuery } from "@apollo/client";
-import { Container, makeStyles } from "@material-ui/core";
-
 import Banner from "../components/Banner";
 import UserBar from "../components/UserBar";
 import { withApollo } from "../lib/context";
 
-const useStyles = makeStyles((theme) => ({
-}));
-
 const Home = () => {
-  const classes = useStyles();
-  // const { loading, error, data } = useQuery(QUERY);
-
   return (
     <>
       <Banner />
       <UserBar />
-      <div>TODO</div>
     </>
   );
 };
--- frontend/pages/groups/[id].js
+++ frontend/pages/groups/[id].js
@@ -1,9 +1,21 @@
 import { gql, useQuery } from "@apollo/client";
 import { useRouter } from "next/router";
+import { Container, makeStyles } from "@material-ui/core";
 import Banner from "../../components/Banner";
 import Group from "../../components/Group";
 import UserBar from "../../components/UserBar";
 import { withApollo } from "../../lib/context";
+
+const useStyles = makeStyles((theme) => ({
+  video: {
+    position: "fixed",
+    bottom: "5vh",
+    right: "5vw",
+    zIndex: 1000,
+    width: "60vw",
+    height: "40vh",
+  },
+}));
 
 const QUERY = gql`
   query Group($id: ID!) {
@@ -18,20 +30,8 @@ const QUERY = gql`
   }
 `;
 
-const QUERYx = gql`
-  {
-    groups {
-      id
-      name
-      description
-      image {
-        formats
-      }
-    }
-  }
-`;
-
 const GroupPage = () => {
+  const classes = useStyles();
   const router = useRouter();
   const { loading, error, data } = useQuery(QUERY, {
     variables: { id: router.query.id },
@@ -41,7 +41,22 @@ const GroupPage = () => {
     <>
       <Banner />
       <UserBar />
-      {!loading && <Group {...data.group} />}
+      <Container maxWidth="lg">
+        {!loading && (
+          <>
+            <Group {...data.group} />
+            <p>
+              Press <strong>S</strong> after joining the meeting to go into
+              full-screen.
+            </p>
+            <iframe
+              className={classes.video}
+              allow="camera; microphone; fullscreen; display-capture"
+              src={`https://meet.jit.si/MeditationGroup${data.group.id}`}
+            />
+          </>
+        )}
+      </Container>
     </>
   );
 };
