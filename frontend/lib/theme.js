9abae1b9a85ecad098907c48fc0e183d8893e2c2
Jāgaro <v.jagaro@gmail.com>
2020-12-25 10:57:16 -0800

    Use default palette (for now)

--- frontend/lib/theme.js
+++ frontend/lib/theme.js
@@ -2,20 +2,20 @@ import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
 
 const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: "#fff",
-    },
-  },
+  // palette: {
+  //   primary: {
+  //     main: "#556cd6",
+  //   },
+  //   secondary: {
+  //     main: "#19857b",
+  //   },
+  //   error: {
+  //     main: red.A400,
+  //   },
+  //   background: {
+  //     default: "#fff",
+  //   },
+  // },
 });
 
 export default theme;
