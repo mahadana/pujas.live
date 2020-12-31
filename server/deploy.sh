c68b4b12ab10dd7a44f7d7ec282ae7305d427799
Jāgaro <v.jagaro@gmail.com>
2020-12-31 03:08:04 -0800

    Optimize restart

--- server/deploy.sh
+++ server/deploy.sh
@@ -17,8 +17,13 @@ log="/var/log/neo.pujas.live-deploy.log"
   }
 
   docker-compose-production build
-  docker-compose-production rm -fs backend frontend worker
+  docker-compose-production stop -t 3 worker
+  docker-compose-production rm -fs worker
+  docker-compose-production stop -t 0 backend frontend
+  docker-compose-production rm -fs backend frontend
+  docker-compose-production up -d db backend frontend
   docker-compose-production up -d
+
   docker image prune -f
 
   echo "$(date) end $0"
