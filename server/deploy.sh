4f2ba1dc6c65aad8de923aeb6eddc635b71af735
Jāgaro <v.jagaro@gmail.com>
2021-01-05 20:15:18 -0800

    Move production docker-compose.yml

--- server/deploy.sh
+++ server/deploy.sh
@@ -12,17 +12,15 @@ log="/var/log/pujas.live-deploy.log"
   cd /opt/pujas.live
   git pull
 
-  function docker-compose-production {
-    docker-compose -f server/docker-compose.production.yml "$@"
-  }
+  cd server
 
-  docker-compose-production build
-  docker-compose-production stop -t 3 worker
-  docker-compose-production rm -fs worker
-  docker-compose-production stop -t 0 backend frontend
-  docker-compose-production rm -fs backend frontend
-  docker-compose-production up -d db backend frontend
-  docker-compose-production up -d
+  docker-compose build
+  docker-compose stop -t 3 worker
+  docker-compose rm -fs worker
+  docker-compose stop -t 0 backend frontend
+  docker-compose rm -fs backend frontend
+  docker-compose up -d db backend frontend
+  docker-compose up -d
 
   docker image prune -f
 
