2663b452c801ffa729da6c494f2bdd1a0fd5453c
jeffzira <jeff.zira@gmail.com>
2021-01-10 18:06:29 -0800

    Floating chating books (#1)
    * WIP, making experimental changes
    
    * Fix video bar and get it functional
    
    * Fix css for chanting book bark
    
    * remove unnecessary changes
    
    Co-authored-by: Jeff Zira <jeff@lark.com>

--- frontend/components/VideoIframeModal.js
+++ frontend/components/VideoIframeModal.js
@@ -6,6 +6,10 @@ import {
   Modal,
 } from "@material-ui/core";
 import CancelIcon from "@material-ui/icons/Cancel";
+import VideoChantingBooksBar from "./VideoChantingBookBar";
+import Slide from '@material-ui/core/Slide';
+import { useState, forwardRef, createRef } from "react";
+
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -30,14 +34,68 @@ const useStyles = makeStyles((theme) => ({
     top: 20,
     right: 20,
   },
+  chantingBooks: {
+    color: "white",
+    position: "absolute",
+
+
+    width: "106px",
+    height: "100%",
+  },
   closeIcon: {
     fontSize: 40,
     color: "white",
+  },
+  hidden: {
+    display:"none",
   },
 }));
 
 const VideoIframeModal = ({ url, open, onClose }) => {
   const classes = useStyles();
+  let timeout;
+
+  const [showing, setShowing] = useState(true);
+  const [hasEnteredOnce, setHasEnteredOnce] = useState(false);
+
+  /**
+    The first time we load the page, show the controls, and then have them move out.
+    This lets the user know that the controls are there, just hiding on the left.
+   */
+  const onBooksEntered = (event) => {
+    if(!hasEnteredOnce) {
+      setTimeout(() => setShowing(false), 1000)
+    }
+    setHasEnteredOnce(true);
+  }
+
+  /**
+   * Show/hide books based on entering/leaving the fake/invisible element (which is exactly the same size)
+   *
+   */
+  const onMouseEnterBooks = (event) => {
+    if(timeout) clearTimeout(timeout)
+    setShowing(true);
+  }
+
+  const onMouseLeaveBooks = (event) => {
+    if(timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => setShowing(false), 500);
+  }
+
+  const onCloseChantingBook = (event) => {
+    setShowing(false)
+  }
+
+  const onClickClose = (event) => {
+    //reset to initial state
+    setHasEnteredOnce(false)
+    setShowing(true)
+
+    onClose() //callback from parent
+  }
+
+  const videoChantingRef = createRef();
 
   return (
     <Modal
@@ -48,9 +106,15 @@ const VideoIframeModal = ({ url, open, onClose }) => {
     >
       <Fade in={open}>
         <Box className={classes.container}>
-          <IconButton className={classes.closeButton} onClick={onClose}>
+          <IconButton className={classes.closeButton} onClick={onClickClose}>
             <CancelIcon className={classes.closeIcon} />
           </IconButton>
+          <Box className={classes.chantingBooks} onMouseEnter={onMouseEnterBooks} onMouseLeave={onMouseLeaveBooks}>
+            <Slide direction="right" in={showing} mountOnEnter unmountOnExit onEntered={onBooksEntered}>
+              <VideoChantingBooksBar ref={videoChantingRef} onClose={onCloseChantingBook} />
+            </Slide>
+          </Box>
+
           <iframe
             src={url}
             seamless="seamless"
