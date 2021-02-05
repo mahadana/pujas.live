aace2febaabe31b70d2ecaa83f127faaf9feb259
Jāgaro <v.jagaro@gmail.com>
2021-02-05 07:59:05 -0800

    Improve developer Docker caching

--- frontend/lib/plausible.js
+++ frontend/lib/plausible.js
@@ -1,5 +1,7 @@
-export const plausibleDomainKey = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN_KEY;
-export const plausibleUrl = process.env.NEXT_PUBLIC_PLAUSIBLE_URL;
+export const plausibleDomainKey =
+  process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN_KEY || "localhost";
+export const plausibleUrl =
+  process.env.NEXT_PUBLIC_PLAUSIBLE_URL || "https://plausible.pujas.live";
 
 const plausible = (eventName, options) => {
   if (typeof window !== "undefined" && window.plausible) {
