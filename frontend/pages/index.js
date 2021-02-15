f67afa1789477b1fb534ca4535559fa69dab9ca1
Jāgaro <v.jagaro@gmail.com>
2021-02-15 00:40:15 -0800

    Use ISG for home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,57 @@
-import { useQuery } from "@apollo/client";
 import { useIdleTimer } from "react-idle-timer";
+import { useEffect, useState } from "react";
 
 import HomeContent from "@/components/HomeContent";
 import PageLayout from "@/components/PageLayout";
-import { withApollo } from "@/lib/apollo";
+import { apolloClient } from "@/lib/apollo";
 import { HOME_QUERY } from "@/lib/schema";
 
-const HomePage = () => {
-  const queryResult = useQuery(HOME_QUERY, {
-    fetchPolicy: "cache-and-network",
+const fetchHomeResult = async () =>
+  await apolloClient.query({
+    fetchPolicy: "network-only",
+    query: HOME_QUERY,
   });
+
+const HomePage = ({ staticResult }) => {
+  const [queryResult, setQueryResult] = useState(null);
+
+  const updateQueryResult = () => {
+    if (queryResult?.loading) {
+      return;
+    }
+
+    setQueryResult({
+      called: true,
+      data: queryResult?.data || staticResult?.data,
+      error: false,
+      loading: true,
+      previousData: queryResult?.previousData,
+    });
+
+    (async () => {
+      let newResult;
+      try {
+        newResult = await fetchHomeResult();
+      } catch (error) {
+        newResult = {
+          called: true,
+          data: queryResult?.data || staticResult?.data,
+          error,
+          loading: false,
+          refetch: updateQueryResult,
+        };
+      }
+      setQueryResult(newResult);
+    })().catch(console.error);
+  };
+
+  useEffect(updateQueryResult, []);
 
   const { reset: resetIdleTimer } = useIdleTimer({
     debounce: 500,
     onIdle: () => {
-      if (!queryResult.error) {
-        queryResult.refetch();
+      if (!queryResult?.error) {
+        updateQueryResult();
       }
       resetIdleTimer();
     },
@@ -23,7 +59,7 @@ const HomePage = () => {
   });
 
   return (
-    <PageLayout chantingBooks queryResult={queryResult}>
+    <PageLayout chantingBooks queryResult={queryResult || staticResult}>
       {({ data }) => (
         <HomeContent channels={data.channels} groups={data.groups} />
       )}
@@ -31,4 +67,22 @@ const HomePage = () => {
   );
 };
 
-export default withApollo({ ssr: true })(HomePage);
+export const getStaticProps = async () => {
+  let staticResult;
+  try {
+    staticResult = await fetchHomeResult();
+  } catch {
+    staticResult = {
+      called: true,
+      data: null,
+      error: false,
+      loading: true,
+    };
+  }
+  return {
+    props: { staticResult },
+    revalidate: 10,
+  };
+};
+
+export default HomePage;
