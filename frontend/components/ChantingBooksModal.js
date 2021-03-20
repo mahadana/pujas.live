1dd1cc7dec21d0bdeb50745d0fcb2237b0f95129
Jāgaro <v.jagaro@gmail.com>
2021-03-19 21:51:40 -0700

    Use new chanting books!

--- frontend/components/ChantingBooksModal.js
+++ frontend/components/ChantingBooksModal.js
@@ -1,17 +1,10 @@
-import Box from "@material-ui/core/Box";
-import Fade from "@material-ui/core/Fade";
-import IconButton from "@material-ui/core/IconButton";
-import Modal from "@material-ui/core/Modal";
-import NoSsr from "@material-ui/core/NoSsr";
-import { makeStyles } from "@material-ui/core/styles";
-import CancelIcon from "@material-ui/icons/Cancel";
 import { createContext, useContext, useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
-import { hasChantDataUrl } from "@/components/chanting/ChantDataUrlContent";
-
-const ChantModal = dynamic(() => import("@/components/chanting/ChantModal"));
+const ChantModal = dynamic(() => import("@/components/chanting/ChantModal"), {
+  ssr: false,
+});
 
 export const ChantingBooksModalContext = createContext({
   open: false,
@@ -21,72 +14,10 @@ export const ChantingBooksModalContext = createContext({
 export const useChantingBooksModal = () =>
   useContext(ChantingBooksModalContext);
 
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: `${theme.zIndex.modal + 2} !important`,
-  },
-  container: {
-    position: "relative",
-    width: "calc(100% - 6em)",
-    height: "calc(100% - 6em)",
-    maxWidth: "70em",
-    backgroundColor: "white",
-  },
-  closeButton: {
-    position: "absolute",
-    top: "-2.1em",
-    right: "-2.1em",
-    color: "rgba(255, 255, 255, 0.9)",
-  },
-  closeIcon: {
-    fontSize: 45,
-  },
-  iframe: {
-    width: "100%",
-    height: "100%",
-  },
-}));
-
-const OldChantModal = ({ onClose, open, state }) => {
-  const classes = useStyles();
-
-  const url = open
-    ? `https://pujas.live/chanting/iframe.html?book=${state?.book}`
-    : null;
-
-  return (
-    <Modal
-      className={classes.modal}
-      closeAfterTransition
-      onClose={onClose}
-      open={open}
-    >
-      <Fade in={open}>
-        <Box className={classes.container}>
-          <IconButton className={classes.closeButton} onClick={onClose}>
-            <CancelIcon className={classes.closeIcon} />
-          </IconButton>
-          <iframe
-            className={classes.iframe}
-            src={url}
-            frameBorder="0"
-            scrolling="no"
-            seamless="seamless"
-          />
-        </Box>
-      </Fade>
-    </Modal>
-  );
-};
-
 const DEFAULT_STATE = {
   book: null,
   inRecording: false,
   onClose: null,
-  newModal: false,
 };
 
 const ChantingBooksModal = ({ children }) => {
@@ -96,8 +27,7 @@ const ChantingBooksModal = ({ children }) => {
   useEffect(() => {
     setState((newState) => ({
       ...newState,
-      inRecording: router.asPath !== "/",
-      newModal: hasChantDataUrl(),
+      inRecording: router.isReady && router.asPath !== "/",
     }));
   }, [router.asPath, setState]);
 
@@ -118,19 +48,14 @@ const ChantingBooksModal = ({ children }) => {
       <ChantingBooksModalContext.Provider value={context}>
         {children}
       </ChantingBooksModalContext.Provider>
-      <NoSsr>
-        {router.isReady && state.newModal ? (
-          <ChantModal
-            disableAudio={state.inRecording}
-            disableFullScreen={state.inRecording}
-            disableReturnToc={state.inRecording}
-            onClose={onClose}
-            open={open}
-          />
-        ) : (
-          <OldChantModal onClose={onClose} open={open} state={state} />
-        )}
-      </NoSsr>
+      <ChantModal
+        book={state.book}
+        disableAudio={state.inRecording}
+        disableFullScreen={state.inRecording}
+        disableReturnToc={state.inRecording}
+        onClose={onClose}
+        open={open}
+      />
     </>
   );
 };
